35eb565fdd6ca0f9ac6c8805eae952e67350463b
Javier Gomez <javiergomezve@gmail.com>
2019-04-25 16:24:11 -0500

    Scaffolding the app

---
+++ src/components/Nightmode.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nightmode = props => (
+  <div>
+    NightMode!!!
+  </div>
+)
+
+export default Nightmode;
--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import WbnPlayer from './WbnPlayer';
+
 const App = () => (
-  <div>Start here!</div>
+  <WbnPlayer />
 )
 
 export default App;
